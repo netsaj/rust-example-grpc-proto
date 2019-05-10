f374cfcc0628499df27594fe5ddf387421e0d72b
Fabio Moreno <fabio.moreno@cuemby.com>
2019-05-10 10:55:39 -0500

    refactor: move rs to compiled folder

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub extern crate futures_cpupool;
 pub extern crate protobuf;
 pub extern crate grpc;
 pub extern crate tls_api;
-//#[path = "../compiled/rust/example.rs"] // Here
+#[path = "../compiled/rust/example.rs"] // found .rs out of src folder
 pub mod example;
-//#[path = "../compiled/rust/example_grpc.rs"] // Here
+#[path = "../compiled/rust/example_grpc.rs"] // found .rs out of src folder
 pub mod example_grpc;
