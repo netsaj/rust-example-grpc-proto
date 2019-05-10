44ef590c3b5213ac85bc33ba60eae4a9b0b01e52
Fabio Moreno <fabio.moreno@cuemby.com>
2019-05-10 10:43:09 -0500

    refactor: move files

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub extern crate futures_cpupool;
 pub extern crate protobuf;
 pub extern crate grpc;
 pub extern crate tls_api;
-#[path = "../compiled/rust/example.rs"] // Here
+//#[path = "../compiled/rust/example.rs"] // Here
 pub mod example;
-#[path = "../compiled/rust/example_grpc.rs"] // Here
+//#[path = "../compiled/rust/example_grpc.rs"] // Here
 pub mod example_grpc;
